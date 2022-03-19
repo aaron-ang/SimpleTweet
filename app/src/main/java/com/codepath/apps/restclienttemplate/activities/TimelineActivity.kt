d45bc11bfd906d0b7e2da473307634c74de80606
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-03-19 11:22:50 -0400

    Change refresh icon color

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.kt
@@ -147,10 +147,7 @@ class TimelineActivity : AppCompatActivity() {
         }
         // Configure the refreshing colors
         swipeContainer.setColorSchemeResources(
-            android.R.color.holo_blue_bright,
-            android.R.color.holo_green_light,
-            android.R.color.holo_orange_light,
-            android.R.color.holo_red_light
+            R.color.twitter_blue,
         )
 
         client = TwitterApplication.getRestClient(this)
